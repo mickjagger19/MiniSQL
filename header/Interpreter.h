a9d82b506eeba7e3a3841e795fb7e60a290a0c77
Mick <mickjagger19@icloud.com>
2018-05-12 15:34:51 +0800

    no message

---
+++ header/Interpreter.h
@@ -0,0 +1,26 @@
+//
+// Created by 钱诚亮 on 2018/5/12.
+//
+
+
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+#include <string>
+#include <vector>
+#include "API.h"
+using namespace std;
+class Interpreter{
+public:
+
+    API* ap;
+    string fileName ;
+    int interpreter(string s);
+
+    string getWord(string s, int *st);
+
+    Interpreter(){}
+    ~Interpreter(){}
+};
+
+#endif
---
+++ source/Attribute.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 钱诚亮 on 2018/5/12.
+//
+
+
+#include "../header/Attribute.h"
+
+Attribute::Attribute(string n, int t, bool i) {
+    name = n;
+    type = t;
+    ifUnique = i;
+    index = "";
+
+}
